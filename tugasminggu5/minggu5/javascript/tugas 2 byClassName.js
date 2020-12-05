d037199d2ff9bf1146b542b1b4a193c850112ed2
HELLA <73866287+rasyidahherawati00@users.noreply.github.com>
2020-12-05 22:53:20 +0700

    Add files via upload

--- tugasminggu5/minggu5/javascript/tugas 2 byClassName.js
+++ tugasminggu5/minggu5/javascript/tugas 2 byClassName.js
@@ -1,1 +1,18 @@
 
+<html lang="en">
+    <head>
+        <title>DOM</title>
+    </head>
+    <body>
+        <p class="kota">Seoul</p>
+        <p class="kota">Vancouver</p>
+        <p class="kota">Amsterdam</p>
+        <p class="kota">Jakarta</p>
+        <p class="kota">Tokyo</p>
+
+        <script>
+            var kota = document.getElementsByClassName("kota");
+            console.log(kota);
+        </script>
+    </body>
+</html>
